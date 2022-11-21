8692f136f2148358fab1ae08623c8bc02b25e5b5
“vncmsk” <yosmarandres@hotmail.com>
2022-11-20 23:31:40 -0500

    Initial commit - Design and integration with email

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+y
+Iapp/src/main/java/com/vncmsk/foonkiemonkey_test/ui/other/BlankFragment.kt,0\4\040bbcfa547c9aa671ce3961dd5a0889542e3ac3
+t
+Dapp/src/main/java/com/vncmsk/foonkiemonkey_test/main/MainActivity.kt,c\5\c5eaed664915b246a3f3577dcce53b87d61d7280
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
---
+++ app/src/main/java/com/vncmsk/foonkiemonkey_test/ui/frontMain/MainFragment.kt
@@ -0,0 +1,54 @@
+package com.vncmsk.foonkiemonkey_test.ui.frontMain
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vncmsk.foonkiemonkey_test.databinding.FragmentMainBinding
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+  private lateinit var binding: FragmentMainBinding
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    binding = FragmentMainBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    button_getInTouch.setOnClickListener {
+      callMail()
+    }
+
+    button_getInTouch2.setOnClickListener {
+      callMail()
+    }
+  }
+
+  private fun callMail() {
+
+    val address = "contact@foonkiemonkey.co.uk"
+    val subject = "I want a quote"
+    val body = "Hi there! I need you to build an application"
+
+    val sendMail = Intent(Intent.ACTION_SEND).apply {
+
+      data = Uri.parse("mailto:")
+
+      putExtra(Intent.EXTRA_EMAIL, arrayOf(address))
+      putExtra(Intent.EXTRA_SUBJECT,subject)
+      putExtra(Intent.EXTRA_TEXT,body)
+    }
+
+    startActivity(sendMail)
+  }
+}
